0c764bad02da284c7478484f02674b3c45bba900
sush1999ingole6925 <sush1999ingole6925@gmail.com>
2026-04-12 04:09:22 +0530

    Added landing page and styling changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { OperatorDetailComponent } from './operator-detail/operator-detail.component';
 import { RxjsOperatorsComponent } from './rxjs-operators/rxjs-operators.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
     { path: 'operators', component: RxjsOperatorsComponent },
     { path: 'operator/:name', component: OperatorDetailComponent },
-    { path: '', redirectTo: 'operators', pathMatch: 'full' }
+    { path: '', redirectTo: '/', pathMatch: 'full' }
 ];
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+  features = [
+    'Visual timelines instead of theory',
+    'Real-time input experimentation',
+    'Understand behavior instantly'
+  ];
+
+  capabilities = [
+    'Play with RxJS operators in real-time',
+    'Modify inputs and see output changes',
+    'Understand timing & async behavior visually',
+    'Build intuition instead of memorizing'
+  ];
+
+  futureItems = [
+    'Operators',
+    'Observables',
+    'Subjects',
+    'Schedulers',
+    'Marble Testing'
+  ];
+}
