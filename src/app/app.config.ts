2631fc3d7573ae708c634f6b858d3e7a50538317
Sushil Ingole <sush1999ingole6925@gmail.com>
2026-04-12 20:38:47 +0530

    fix: update scroll position restoration setting for router configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,5 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' }))]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }))]
 };
